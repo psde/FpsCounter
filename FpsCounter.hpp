9e56607d119e608f13bd999b165eb9def1f591a3
psde <me@mathias-garbe.de>
2010-02-09 17:18:42 +0100

    added commentary and fixes

--- FpsCounter.hpp
+++ FpsCounter.hpp
@@ -10,28 +10,31 @@
 #include <deque>
 #include <math.h>
 
-struct Point
+namespace
 {
-	Point(int x = 0, int y = 0) {
-		this->x = x;
-		this->y = y;
-	}
-	int x,y;
-};
-
-struct FpsPoint
-{
-	FpsPoint(double fps, bool landmark = false)
+	struct Point
 	{
-		this->fps = fps;
-		this->landmark = landmark;
-	}
+		Point(int x = 0, int y = 0) {
+			this->x = x;
+			this->y = y;
+		}
+		int x,y;
+	};
 
-	double fps;
-	bool landmark;
-};
+	struct FpsPoint
+	{
+		FpsPoint(double fps, bool landmark = false)
+		{
+			this->fps = fps;
+			this->landmark = landmark;
+		}
 
-namespace Gosu
+		double fps;
+		bool landmark;
+	};
+}
+
+namespace GosuEx
 {
 	class FpsCounter
 	{
@@ -49,12 +52,8 @@ namespace Gosu
 		
 		Gosu::Color boxColor, lineColor, lineLandmarkColor;
 
-		float updateInterval;
-		float accum;
-		int frames;
-		float timeleft;
-		int lastFrameCompleted;
-		double fps;
+		double updateInterval, accum, timeleft, fps;
+		int frames, lastFrameCompleted;
 
 	public:	
 		FpsCounter(Gosu::Graphics *graphics, int x, int y, int width, int height, float updateInterval = 0.03f)
@@ -79,19 +78,22 @@ namespace Gosu
             this->updateInterval = interval;
         }
 
+		// this yields wrong results if not called on the end of the draw() method, so maybe we can just 
+		// do the calulation in FpsCounter::draw (user has only one function to call every time, not two)
+		// on the other side: what about user that want to calculate but not want to show them? Hmm.. think about that
 		void updateFPS()
 		{
 			// based on http://www.unifycommunity.com/wiki/index.php?title=FramesPerSecond
-			float deltaTime = (float)Gosu::milliseconds() - this->lastFrameCompleted;
+			double deltaTime = Gosu::milliseconds() - this->lastFrameCompleted;
 			this->timeleft -= deltaTime/1000.0f;
 			this->accum += 1000.0f/deltaTime;
 			this->frames++;
-			this->fps = round(accum/frames);
+			this->fps = Gosu::round(accum/frames);
 
 			if( timeleft <= 0.0 )
 			{
 				bool landmark = false;
-				if(Gosu::milliseconds() > (unsigned int)logtime + 1000)
+				if(Gosu::milliseconds() > static_cast<unsigned int>(logtime + 1000))
 				{
 					logtime = Gosu::milliseconds();
 					landmark = true;
@@ -102,7 +104,7 @@ namespace Gosu
 				this->timeleft = this->updateInterval;
 			}
 
-			while(fpslist.size() > (unsigned int)this->width+1)
+			while(fpslist.size() > static_cast<unsigned int>(this->width+1))
 				fpslist.pop_front();
 		}
 
@@ -113,18 +115,16 @@ namespace Gosu
 			Point topLeft(x, y);
 			Point downRight(x + width, y + height);
 
-			int z = 10000;
+			// draw box 
+			this->graphics->drawLine(topLeft.x, topLeft.y, boxColor, downRight.x, topLeft.y, boxColor, Gosu::zImmediate); // Top 
+			this->graphics->drawLine(topLeft.x - 1, downRight.y, boxColor, downRight.x, downRight.y, boxColor, Gosu::zImmediate); // Bottom 
+			this->graphics->drawLine(topLeft.x, topLeft.y, boxColor, topLeft.x, downRight.y, boxColor, Gosu::zImmediate); // Left 
+			this->graphics->drawLine(downRight.x, topLeft.y, boxColor, downRight.x, downRight.y, boxColor, Gosu::zImmediate); // Left
 
-			// Draw box 
-			this->graphics->drawLine(topLeft.x, topLeft.y, boxColor, downRight.x, topLeft.y, boxColor, z); // Top 
-			this->graphics->drawLine(topLeft.x - 1, downRight.y, boxColor, downRight.x, downRight.y, boxColor, z); // Bottom 
-			this->graphics->drawLine(topLeft.x, topLeft.y, boxColor, topLeft.x, downRight.y, boxColor, z); // Left 
-			this->graphics->drawLine(downRight.x, topLeft.y, boxColor, downRight.x, downRight.y, boxColor, z); // Left
+			// draw fps 
+			font->draw(L"Cur: " + boost::lexical_cast<std::wstring>(fps), topLeft.x+4, topLeft.y+2, Gosu::zImmediate, 1, 1, this->boxColor);
 
-			// Draw fps 
-			font->draw(L"Cur: " + boost::lexical_cast<std::wstring>(fps), topLeft.x+4, topLeft.y+2, z+1, 1, 1, this->boxColor);
-
-			// Draw graph, if we are on it, calc avg fps 
+			// draw graph, if we are on it, calculate avg fps 
 			double avg = 0;
 			int avgCount = 0;
 			if(fpslist.size() > 2)
@@ -135,17 +135,17 @@ namespace Gosu
 					double val = fpslist.at(i).fps * -1 + height;
 					double val2 = fpslist.at(i+1).fps * -1 + height;
 
-					val = clamp(val, 0.0, (double)height);
-					val2 = clamp(val2, 0.0, (double)height);
+					val = Gosu::clamp(val, 0.0, static_cast<double>(height));
+					val2 = Gosu::clamp(val2, 0.0, static_cast<double>(height));
 					
-					this->graphics->drawLine(x, topLeft.y + val2, lineColor, x+1, topLeft.y + val, lineColor, z - 1);
+					this->graphics->drawLine(x, topLeft.y + val2, lineColor, x+1, topLeft.y + val, lineColor, Gosu::zImmediate);
 					
 					if(fpslist.at(i).landmark)
 					{
-						this->graphics->drawLine(x, topLeft.y + height, lineLandmarkColor, x+1, topLeft.y + val, lineLandmarkColor, z - 1);
+						this->graphics->drawLine(x, topLeft.y + height, lineLandmarkColor, x+1, topLeft.y + val, lineLandmarkColor, Gosu::zImmediate);
 					}
 
-					// Ignore negative fps, only happens a few times after init
+					// ignore negative fps, only happens a few times after init
 					if(fpslist.at(i).fps > 0)
 					{
 						avg += fpslist.at(i).fps;
@@ -154,8 +154,8 @@ namespace Gosu
 				}
 				avg = avg / avgCount;
 
-
-				font->draw(L"Avg: " + boost::lexical_cast<std::wstring>(avg).substr(0,4), topLeft.x+4, topLeft.y+14, z+1, 1, 1, this->boxColor);
+				// maybe we should round the avg fps instead of just taking the first 4 chars
+				font->draw(L"Avg: " + boost::lexical_cast<std::wstring>(avg).substr(0,4), topLeft.x+4, topLeft.y+14, Gosu::zImmediate, 1, 1, this->boxColor);
 			}
 		}
 	};
